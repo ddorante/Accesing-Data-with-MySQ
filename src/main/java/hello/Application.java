d55be793c34dcdf3a5c65474c3d09601d2cc8df1
ddorante <Xikitoelmejor_88>
2018-09-06 12:46:09 +0200

    Corregido

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,8 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
